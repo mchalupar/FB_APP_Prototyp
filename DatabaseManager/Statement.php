5483394816e0a75e40502aaf1190367e6d715174
mchalupar <s1010307052@students.fh-hagenberg.at>
2013-02-01 00:34:47 +0100

    added Statement Class and UnitTest

---
+++ DatabaseManager/Statement.php
@@ -0,0 +1,70 @@
+<?php
+
+include_once '../Model/InvalidFormatException.php';
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Charly
+ * Date: 31.01.13
+ * Time: 23:31
+ * To change this template use File | Settings | File Templates.
+ */
+class Statement
+{
+    private $stmt = '';
+
+    public function prepare($sql)
+    {
+        if (is_string($sql))
+            $this->stmt = $sql;
+        else
+            throw new InvalidFormatException();
+    }
+
+    public function bindParam($param, $value, $type)
+    {
+        if (!is_string($param))
+            throw new InvalidFormatException();
+        if (!is_string($type))
+            throw new InvalidFormatException();
+
+        switch($type)
+        {
+            case "string":
+                if (!is_string($value))
+                    throw new InvalidFormatException();
+                else
+                    $value = (string)$value;
+                break;
+
+            case "integer":
+                if (!is_integer($value))
+                    throw new InvalidFormatException();
+                else
+                    $value = (string)$value;
+                break;
+
+            case "boolean":
+                if (!is_bool($value))
+                    throw new InvalidFormatException();
+                else {
+                    $value = (string)$value;
+                    if ($value == "1") $value = "true";
+                    else $value = "false";
+                }
+                break;
+
+            default:
+                throw new InvalidFormatException();
+                break;
+        }
+        $this->stmt = str_replace($param, $value, $this->stmt);
+    }
+
+    public function __toString()
+    {
+        return $this->stmt;
+    }
+
+
+}
---
+++ UnitTests/StatementTest.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once("../DatabaseManager/Statement.php");
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Charly
+ * Date: 31.01.13
+ * Time: 23:49
+ * To change this template use File | Settings | File Templates.
+ */
+
+$stmt = new Statement();
+$stmt->prepare("SELECT * FROM :table WHERE :column = :value");
+echo $stmt."</br>";
+$stmt->bindParam(":table", "MyTable", "string" );
+echo $stmt."</br>";
+$stmt->bindParam(":column", "MyColumn", "string");
+echo $stmt."</br>";
+$stmt->bindParam(":value", false, "boolean");
+echo $stmt."</br>";
