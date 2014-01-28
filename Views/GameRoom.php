df5f10b59c928622e7cc382e27949cf611e32204
mchalupar <s1010307052@students.fh-hagenberg.at>
2014-01-28 17:44:42 +0100

    Added first View - GameRoom - which is the main Application

--- Views/GameRoom.php
+++ Views/GameRoom.php
@@ -1,1 +1,40 @@
 <?php
+
+
+?>
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="css/gameroom.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Kotta+One' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div class="wrapper">
+    <header id="profile">
+        <h2>Profile: Username</h2>
+        <p>Score: 999</p>
+        <p>Level: 99</p>
+        <img src="/" alt="No Profile Pic">
+    </header>
+
+    <section id="challenge">
+        <article class="question">
+            <h1>What is 2 square 3?</h1>
+            <p>About square roots: <a href="http://google.at/search?q=square+roots">Inform yourself!!</a></p>
+        </article>
+
+        <div class="options">
+            <a class="option" state="unchecked">2</a>
+            <div class="option" state="unchecked">3</div>
+            <div class="option" state="unchecked">4</div>
+            <div class="option" state="unchecked">6</div>
+        </div>
+    </section>
+
+    <footer id="progress">
+
+    </footer>
+
+
+</div>
+</body>
+</html>
